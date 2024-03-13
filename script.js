e0756fb6eb8989c521faa1c8d2bb1aa9d7e6b3f5
Door <doriamfloreszorrilla@gmail.com>
2024-03-12 19:22:54 -0500

    ADD first page

---
+++ script.js
@@ -0,0 +1,5 @@
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = () => {
+  navBar = document.querySelector(".navbar");
+  navBar.classList.toggle("active");
+};
